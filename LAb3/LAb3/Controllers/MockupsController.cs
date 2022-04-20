205450031771cf0358ded3d8f7eebc5793d3fee9
onsfyou <81429417+onsfyou@users.noreply.github.com>
2022-04-20 14:15:03 +0700

    LAb3

---
+++ LAb3/LAb3/Controllers/MockupsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAb3.Models;
+namespace LAb3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        static Quiz quiz = new();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Quiz(int? Answer, string Action)
+        {
+            if (Answer != null)
+                quiz.AddUserAnswer((int)Answer);
+
+            if (Action == "Finish")
+                return RedirectToAction("QuizResult");
+
+            if (Action != "Next")
+            {
+                quiz.ClearQuiz();
+            }
+
+            //int question = quiz.QuesNumber();
+            quiz.CreateQuestion();
+            ViewData["Num1"] = quiz.Number1[quiz.QuestionNumber];
+            ViewData["Num2"] = quiz.Number2[quiz.QuestionNumber];
+            ViewData["Oper"] = quiz.Operation[quiz.QuestionNumber];
+
+            return View();
+        }
+        public IActionResult QuizResult()
+        {
+            if (quiz.IsValid())
+            {
+                ViewData["QuizValid"] = true;
+                ViewData["Num1"] = quiz.Number1;
+                ViewData["Num2"] = quiz.Number2;
+                ViewData["Oper"] = quiz.Operation;
+                ViewData["UserAnswers"] = quiz.UserAnswer;
+                ViewData["CorrectAnswersAmount"] = quiz.CorrectAnswersAmount();
+                ViewData["QuestionsAmount"] = quiz.QuestionNumber + 1;
+            }
+            else
+                ViewData["QuizValid"] = false;
+            return View();
+        }
+    }
+}
---
+++ LAb3/LAb3/Models/Quiz.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAb3.Models
+{
+    public class Quiz
+    {
+        public List<int> Number1 = new();
+        public List<int> Number2 = new();
+        public List<string> Operation = new();
+        public List<int> RightAnswer = new();
+        public List<int> UserAnswer = new();
+        public int QuestionNumber = -1;
+
+        public void CreateQuestion()
+        {
+            QuestionNumber++;
+            Random rnd = new Random();
+            Number1.Add(rnd.Next(0, 10));
+            Number2.Add(rnd.Next(1, 10));
+            int a = rnd.Next(0, 4);
+            switch (a)
+            {
+                case 0:
+                    Operation.Add("+");
+                    RightAnswer.Add(Number1[QuestionNumber] + Number2[QuestionNumber]);
+                    break;
+                case 1:
+                    Operation.Add("-");
+                    RightAnswer.Add(Number1[QuestionNumber] - Number2[QuestionNumber]);
+                    break;
+                case 2:
+                    Operation.Add("*");
+                    RightAnswer.Add(Number1[QuestionNumber] * Number2[QuestionNumber]);
+                    break;
+                case 3:
+                    Operation.Add("/");
+                    RightAnswer.Add(Number1[QuestionNumber] / Number2[QuestionNumber]);
+                    break;
+            }
+        }
+        public void AddUserAnswer(int Answer)
+        {
+            UserAnswer.Add(Answer);
+        }
+        public void ClearQuiz()
+        {
+            Number1.Clear();
+            Number2.Clear();
+            Operation.Clear();
+            RightAnswer.Clear();
+            UserAnswer.Clear();
+            QuestionNumber = -1;
+        }
+        //public int QuesNumber()
+        //{
+        //    return QuestionNumber;
+        //}
+        public bool IsValid()
+        {
+            return (RightAnswer.Count == UserAnswer.Count);
+        }
+        public int CorrectAnswersAmount()
+        {
+            int amount = 0;
+            for (int i = 0; i < QuestionNumber + 1; i++)
+                if (RightAnswer[i] == UserAnswer[i])
+                    amount++;
+            return amount;
+        }
+    }       
+}
